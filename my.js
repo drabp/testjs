5097b7223e40edfdaf40faf9d12961e48a34c031
drabp <avaniplast@yahoo.co.in>
2021-05-29 10:42:54 +0530

    Update my.js

--- my.js
+++ my.js
@@ -1,5 +1,5 @@
 function testGS(){
-    const url = "https://script.google.com/macros/s/AKfycbzknMy3YxOC2kB0txl5A5KjvV5m20oJDgHlLqiHZN18jWhxEJuw-GN9TyoyW95jSrvS/exec";
+    const url = "https://script.google.com/macros/s/AKfycbxbzZtBxcoH0Lh6XkZur4DTY4MvPbiSGg6yRkOOsxt8GAB2XJsfM4UgkCPy9lY1bkAS/exec";
     fetch(url)
     .then(d => d.json())
     .then(d => {
@@ -7,4 +7,24 @@ function testGS(){
     
     });
 }
+
+function addGS(){
+    const url = "https://script.google.com/macros/s/AKfycbxbzZtBxcoH0Lh6XkZur4DTY4MvPbiSGg6yRkOOsxt8GAB2XJsfM4UgkCPy9lY1bkAS/exec";
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'no-cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'omit', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify({name:"Deepak"}) // body data type must match "Content-Type" header
+
+    });
+    console.log('done');
+}
+document.getElementById("btn2").addEventListener("click",addGS);
 document.getElementById("btn").addEventListener("click",testGS);
